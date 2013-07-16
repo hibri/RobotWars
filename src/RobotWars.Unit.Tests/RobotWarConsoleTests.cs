54a4e5ce3881478fe080b9bb67ea0ffacd890d27
Hibri Marzook <hibri@hibri.net>
2013-07-16 10:48:49 +0100

    First commit, first test

---
+++ src/RobotWars.Unit.Tests/RobotWarConsoleTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace RobotWars.Unit.Tests
+{
+    [TestFixture]
+	public class RobotWarConsoleTests
+    {
+	    [Test]
+	    public void Robot_should_report_current_position() {
+		    
+			Assert.That(Input("1 2 N"), Is.EqualTo("1 2 N"));
+	    }
+
+	    private string Input(string command) {
+		    return command;
+	    }
+    }
+}
