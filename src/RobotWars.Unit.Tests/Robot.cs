256c0cec79c912dc5f6aa1bc9c6490b842622ac4
Hibri Marzook <hibri@hibri.net>
2013-07-16 11:20:06 +0100

    Make a robot class to parse initial position commands only

---
+++ src/RobotWars.Unit.Tests/Robot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RobotWars.Unit.Tests
+{
+	internal class Robot
+	{
+		public void ParseCommands(string[] command) {
+			string[] positionCommandParts = command[0].Split(' ');
+			X = Int32.Parse(positionCommandParts[0]);
+			Y = Int32.Parse(positionCommandParts[1]);
+			Heading = positionCommandParts[2];
+		}
+
+		public int X { get; set; }
+		public int Y { get; set; }
+		public string Heading { get; set; }
+
+		public string GetCurrentPosition() {
+			return string.Format("{0} {1} {2}", X, Y , Heading);
+		}
+	}
+}
--- src/RobotWars.Unit.Tests/RobotWarConsoleTests.cs
+++ src/RobotWars.Unit.Tests/RobotWarConsoleTests.cs
@@ -8,11 +8,41 @@ namespace RobotWars.Unit.Tests
 	    [Test]
 	    public void Robot_should_report_current_position() {
 		    
-			Assert.That(Input("1 2 N"), Is.EqualTo("1 2 N"));
+			var robot1 = new Robot();
+		    
+			robot1.ParseCommands(new []{"1 2 N"});
+
+		    Assert.That(robot1.GetCurrentPosition(), Is.EqualTo("1 2 N"));
 	    }
 
-	    private string Input(string command) {
-		    return command;
+	    [Test]
+	    public void Should_parse_X_pos_from_an_initial_position_command() {
+		    
+			var robot = new Robot();
+		    
+			robot.ParseCommands(new []{"1 2 N"});
+
+		    Assert.That(robot.X, Is.EqualTo(1));
 	    }
+
+	    [Test]
+		public void Should_parse_Y_pos_from_an_initial_position_command() {
+		    
+			var robot = new Robot();
+
+		    robot.ParseCommands(new[] { "1 2 N" });
+
+		    Assert.That(robot.Y, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void Should_parse_current_heading_from_an_initial_position_command()
+		{
+			var robot = new Robot();
+			
+			robot.ParseCommands(new[] { "1 2 N" });
+
+			Assert.That(robot.Heading, Is.EqualTo("N"));
+		}
     }
 }
